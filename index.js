25ef4e4966847881c4f62d4adf1fc14f19a31022
Treagan Birbal <t.birbal@gmail.com>
2018-04-02 19:09:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var name = "Joe";
 var height = "74";
-if (name === Susan){
-  console.log("The name is correct");
-}
-Else {
+if (name === Susan) {
+  console.log("The name is correct")
+} Else {
   console.log("Expected the name Susan");
 }
 
